80d82999eb99c4b6ca17f8a6026c752bb50e8e6d
2209407652 <2209407652@qq.com>
2022-03-13 20:14:42 +0800

    初始化并梳理项目的结构

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 完整导入并注册 vant 组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
+// 引入 amfe-flexible
+import 'amfe-flexible'
+
 Vue.config.productionTip = false
 
 new Vue({
